ce826db5594729068602725b249ce5b80c671578
Duvauwn Prendergast <duvauwnprendergast@gmail.com>
2021-11-11 22:56:42 +0000

    fixing timer

--- script.js
+++ script.js
@@ -1,30 +1,40 @@
 let second = document.querySelector('.second');
 let minute = document.querySelector('.minute');
+let user = document.querySelector('#user');
+let colon = document.querySelector('#colon');
 
 let button = document.querySelector('#start');
 
 let strS = parseInt(second.textContent);
-let strM = parseInt(minute.textContent);
+let strM = minute.textContent;
 
 let start;
-let end;
 
 function deprecate() {
+
     strS--;
-    console.log(strS);
-    second.textContent = strS;
+    if (strS < 10) {
+        second.textContent = '0' + strS;
+    }
+    else {
+        second.textContent = strS;
+    }
+
     if (strS == 0) {
         strS = 60;
-        strM--
+        strM--;
         minute.textContent = strM;
     }
     return strS;
-}
+};
 
 button.addEventListener('click', function () {
+
+    minute.textContent = parseInt(user.value);
+    minute.textContent--;
+    colon.textContent = ':';
+    strM = minute.textContent;
     button.disabled = true;
-    start = setInterval(deprecate, 1000)
-})
-
-
-console.log(strS);
+    strS = 60;
+    start = setInterval(deprecate, 1000);
+});
